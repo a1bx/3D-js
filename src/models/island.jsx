86f23fccd18cbe9c4675fddf268be9d548057c91
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2023-11-23 21:00:55 +0300

    update

--- src/models/island.jsx
+++ src/models/island.jsx
@@ -3,11 +3,11 @@ import { useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { a } from "@react-spring/three";
 
- import islandScene from "../assets/3d//islandscene.glb";
+ import islandScene from "../assets/3d//island.glb";
 
 const Island =(props)=>{
   const islandRef = useRef();
-  const { nodes, materials } = useGLTF("/island.glb");
+  const { nodes, materials } = useGLTF(islandScene);
   return (
     <a.group ref={islandRef}{...props}>
       <group

75eff5e10689ba8c824b2983265d26fb8ba793f2
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2023-11-23 19:50:48 +0300

    loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/loader'
 
 const home = () => {
   return (
     <section className='w-full h-screen relative' >
-      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         pop
-      </div>
+      </div> */}
+      <Canvas
+        className='w-full h-full bg-transparent'
+        camera={{ near: 0.1, far: 1000}}
+      >
+        <Suspense fallback={<Loader/>}>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} />
+        </Suspense>
+      </Canvas>
     </section>
   )
 }

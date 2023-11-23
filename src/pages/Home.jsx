a19bb0f3750bddceb5de53a42dc62e1413632b3a
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2023-11-23 22:34:37 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,38 @@ import Loader from '../components/loader'
 import Island from '../models/island'
 
 const home = () => {
+  // const adjustBiplaneForScreenSize = () => {
+  //   let screenScale, screenPosition;
+
+  //   // If screen width is less than 768px, adjust the scale and position
+  //   if (window.innerWidth < 768) {
+  //     screenScale = [1.5, 1.5, 1.5];
+  //     screenPosition = [0, -1.5, 0];
+  //   } else {
+  //     screenScale = [3, 3, 3];
+  //     screenPosition = [0, -4, -4];
+  //   }
+
+  //   return [screenScale, screenPosition];
+  // };
+  const adjustScreen = () => {
+    // let screenScale=null;
+    // let screenPosition=[0,-6.5,-43]
+    // let screenRotation=[0.1,4.7,0]
+    let screenScale,screenPosition,screenRotation;
+
+    if (window.innerWidth < 768) {
+       screenScale=[0.9,0.9,0.9];
+       screenPosition=[0,-6.5,-43];
+    } else {
+      screenScale=[1,1,1];
+       screenPosition=[0,-6.5,-43];
+    }
+    return [screenScale,screenPosition,screenRotation];
+  }
+
+  const [islandScale,islandPosition,islandRotation]=adjustScreen();
+
   return (
     <section className='w-full h-screen relative' >
       {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -19,7 +51,11 @@ const home = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight />
-          <Island/>
+          <Island
+            position={islandPosition}
+            scale={islandScale}  
+            rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>

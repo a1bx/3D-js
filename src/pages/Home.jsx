6909b4125a770324addf314392343cd2ea7221f1
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2023-11-25 22:46:08 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,65 +1,119 @@
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import Loader from '../components/loader'
-import Island from '../models/island'
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
 
-const home = () => {
-  // const adjustBiplaneForScreenSize = () => {
-  //   let screenScale, screenPosition;
+import sakura from "../assets/sakura.mp3";
+import { HomeInfo, Loader } from "../components";
+import { soundoff, soundon } from "../assets/icons";
+import { Bird, Island, Plane, Sky } from "../models";
 
-  //   // If screen width is less than 768px, adjust the scale and position
-  //   if (window.innerWidth < 768) {
-  //     screenScale = [1.5, 1.5, 1.5];
-  //     screenPosition = [0, -1.5, 0];
-  //   } else {
-  //     screenScale = [3, 3, 3];
-  //     screenPosition = [0, -4, -4];
-  //   }
+const Home = () => {
+  const audioRef = useRef(new Audio(sakura));
+  audioRef.current.volume = 0.4;
+  audioRef.current.loop = true;
 
-  //   return [screenScale, screenPosition];
-  // };
-  const adjustScreen = () => {
-    // let screenScale=null;
-    // let screenPosition=[0,-6.5,-43]
-    // let screenRotation=[0.1,4.7,0]
-    let screenScale,screenPosition,screenRotation;
+  const [currentStage, setCurrentStage] = useState(1);
+  const [isRotating, setIsRotating] = useState(false);
+  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+
+  useEffect(() => {
+    if (isPlayingMusic) {
+      audioRef.current.play();
+    }
+
+    return () => {
+      audioRef.current.pause();
+    };
+  }, [isPlayingMusic]);
+
+  const adjustBiplaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    // If screen width is less than 768px, adjust the scale and position
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale, screenPosition;
 
     if (window.innerWidth < 768) {
-       screenScale=[0.9,0.9,0.9];
-       screenPosition=[0,-6.5,-43];
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43.4];
     } else {
-      screenScale=[1,1,1];
-       screenPosition=[0,-6.5,-43];
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43.4];
     }
-    return [screenScale,screenPosition,screenRotation];
-  }
 
-  const [islandScale,islandPosition,islandRotation]=adjustScreen();
+    return [screenScale, screenPosition];
+  };
+
+  const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
 
   return (
-    <section className='w-full h-screen relative' >
-      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-        pop
-      </div> */}
+    <section className='w-full h-screen relative'>
+      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        {currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
+
       <Canvas
-        className='w-full h-full bg-transparent'
-        camera={{ near: 0.1, far: 1000}}
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
+        camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader/>}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 5, 10]} intensity={2} />
+          <spotLight
+            position={[0, 50, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+          <hemisphereLight
+            skyColor='#b1e1ff'
+            groundColor='#000000'
+            intensity={1}
+          />
+
+          <Bird />
+          <Sky isRotating={isRotating} />
           <Island
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
             position={islandPosition}
-            scale={islandScale}  
-            rotation={islandRotation}
+            rotation={[0.1, 4.7077, 0]}
+            scale={islandScale}
+          />
+          <Plane
+            isRotating={isRotating}
+            position={biplanePosition}
+            rotation={[0, 20.1, 0]}
+            scale={biplaneScale}
           />
         </Suspense>
       </Canvas>
-    </section>
-  )
-}
 
-export default home
+      <div className='absolute bottom-2 left-2'>
+        <img
+          src={!isPlayingMusic ? soundoff : soundon}
+          alt='jukebox'
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+          className='w-10 h-10 cursor-pointer object-contain'
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
